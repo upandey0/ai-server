fd919a07073e05c9f0578f660ced2ba4e7812dd0
upandey0 <rubusiness0@gmail.com>
2024-04-11 18:35:21 +0530

    3

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import morgan from 'morgan'
 import userRouter from './routes/user.routes.js';
 import companyRoutes from './routes/user.company.routes.js';
 import reportRoutes from './routes/report.routes.js';
 import fileRoutes from './routes/fileRoutes.js';
 
 const app = express();
-
+app.use(morgan('dev'));
+app.use(
+    cors({
+      origin: ['https://fa-ai-client-dashboard.vercel.app/*'],
+      credentials: true,
+    })
+  );
+  
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: '32kb' }));
 app.use(express.static('public'));
 
-app.use(
-  cors({
-    origin: ['https://fa-ai-client-dashboard.vercel.app/*'],
-    credentials: true,
-  })
-);
 
 app.use('/api/user', userRouter);
 app.use('/api/companies', companyRoutes);
